1b5ff091c35b9178824322c058dc391564fe836c
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-09-22 01:03:01 +0200

    test: write test scaffold

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,46 @@
+const { Application } = require("probot");
+
 // You can import your modules
-// import index from '../src/index'
+import index from "../src/index";
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
-})
+describe("All-Contributors-Bot", () => {
+  let app, github;
 
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
+  beforeEach(() => {
+    app = new Application();
+
+    app.load(index);
+
+    github = {};
+    // Passes the mocked out GitHub API into out app instance
+    app.auth = () => Promise.resolve(github);
+  });
+
+  describe("config", () => {
+    it.skip("loads a config");
+  });
+
+  describe("modes", () => {
+    describe("issue", () => {
+      it.skip(
+        "creates an issue if a committer is not in the list contributor list"
+      );
+      it.skip(
+        "creates no issue if all committer are in the list contributor list"
+      );
+
+      it.skip("ignores blacklisted names");
+    });
+
+    describe("commit", () => {
+      it.skip(
+        "creates and pushes a commit if a committer is not in the list contributor list"
+      );
+      it.skip(
+        "creates no commit if all committer are in the list contributor list"
+      );
+
+      it.skip("ignores blacklisted names");
+    });
+  });
+});
